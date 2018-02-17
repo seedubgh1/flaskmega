1ee177f7abd3de04e288eb8cbd87df2eb24e37b5
seedubgh1 <carltonsaddress@hotmail.com>
2018-02-16 17:15:35 -0800

    added existing code

---
+++ app/__init__.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
+#### EMAIL SERVER PARAMS - CONFIGURE THESE AS ENVIRONMENT VARS ####
+mailserver = app.config['MAIL_SERVER']
+mailusername = app.config['MAIL_USERNAME']
+mailpasswd = app.config['MAIL_PASSWORD']
+mailusetls = app.config['MAIL_USE_TLS']
+mailport = app.config['MAIL_PORT']
+adminemails = app.config['ADMIN_EMAILS']
+
+if not app.debug:
+	#### CONFIGURE SMTP EMAIL HANDLER TO REPORT ERRORS ####
+	if mailserver:
+		auth = None
+		if mailusername or mailpasswd:
+			auth = (mailusername, mailpasswd)
+		secure = None
+		if mailusetls:
+			secure = ()
+		mail_handler = SMTPHandler(
+			mailhost=(mailserver, mailport),
+			fromaddr='no-reply@' + mailserver,
+			toaddrs=adminemails,
+			credentials=auth,
+			secure=secure,
+			subject='An error was logged...')
+		mail_handler.setLevel(logging.ERROR)
+		app.logger.addHandler(mail_handler)
+
+	#### CONFIGURE FILE HANDLER TO LOG ERRORS ####
+	if not os.path.exists('logs'):
+		os.mkdir('logs')
+	file_handler = RotatingFileHandler(
+		'logs/microblog.log',
+		maxBytes=10240,
+		backupCount=10)
+	file_handler.setFormatter(
+		logging.Formatter(
+			'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+		)
+	)
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+
+	app.logger.setLevel(logging.INFO)
+	app.logger.info("Microblog startup")
+
+from app import routes, models, errors
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'guess-this-and-you-win'
+
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = os.environ.get('MAIL_PORT')
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMIN_EMAILS = ['carltonsaddress@hotmail.com']
