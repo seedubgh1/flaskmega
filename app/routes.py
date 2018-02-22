291d1c689a573b462a8262a0081f810ee164e347
Carlton <carltonsaddress@hotmail.com>
2018-02-21 18:28:49 -0800

    added pagination links to index.html, before '.. for User Profile'

--- app/routes.py
+++ app/routes.py
@@ -24,8 +24,14 @@ def index():
 		db.session.commit()
 		flash('Your post is now live!')
 		return redirect(url_for('index'))
-	posts = current_user.followed_posts().all()
-	return render_template('index.html', title='Home Page', form=form, posts=posts)
+	page = request.args.get('page', 1, type=int)
+	# posts = current_user.followed_posts().all()
+	posts = current_user.followed_posts().paginate(
+		page, app.config['POSTS_PER_PAGE'], False)
+	next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+	prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+	return render_template('index.html', title='Home Page', form=form, posts=posts.items,
+		next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -67,10 +73,7 @@ def register():
 @login_required
 def user(username):
 	user = User.query.filter_by(username=username).first_or_404()
-	posts = [
-		{'author': user, 'body': 'Test post #1'},
-		{'author': user, 'body': 'Test post #2'}
-	]
+
 	posts = Post.query.filter_by(user_id=user.id)
 	return render_template('user.html', user=user, posts=posts)
 
@@ -122,5 +125,11 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
+	page = request.args.get('page', 1, type=int)
 	posts = Post.query.order_by(Post.timestamp.desc()).all()
-	return render_template('index.html', title='Explore', posts=posts)
+	posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+		page, app.config['POSTS_PER_PAGE'], False)
+	next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+	prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+	return render_template('index.html', title='Explore', posts=posts.items,
+		next_url=next_url, prev_url=prev_url)
--- config.py
+++ config.py
@@ -13,3 +13,5 @@ class Config(object):
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMIN_EMAILS = ['carltonsaddress@hotmail.com']
+
+	POSTS_PER_PAGE = 3
