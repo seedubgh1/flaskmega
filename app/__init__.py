aad5eacc9d15777eb449164d09968d416113d936
Carlton <carltonsaddress@hotmail.com>
2018-03-13 18:35:07 -0700

    intermediate commit of chapter 15

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -11,66 +11,86 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
 login.login_message = _l('Please log in to access this page')
-mail = Mail(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+mail = Mail()
+bootstrap = Bootstrap()
+moment = Moment()
 babel = Babel(app)
 
-#### EMAIL SERVER PARAMS - CONFIGURE THESE AS ENVIRONMENT VARS ####
-mailserver = app.config['MAIL_SERVER']
-mailusername = app.config['MAIL_USERNAME']
-mailpasswd = app.config['MAIL_PASSWORD']
-mailusetls = app.config['MAIL_USE_TLS']
-mailport = app.config['MAIL_PORT']
-adminemails = app.config['ADMIN_EMAILS']
+def create_app(config_class=Config):
+	app = Flask(__name__)
+	app.config.from_object(Config)
+	db.init_app(app)
+	migrate.init_app(app)
+	login.init_app(app)
+	mail.init_app(app)
+	boostrap.init_app(app)
+	moment.init_app(app)
+	babel.init_app(app)
 
-if not app.debug:
-	#### CONFIGURE SMTP EMAIL HANDLER TO REPORT ERRORS ####
-	if mailserver:
-		auth = None
-		if mailusername or mailpasswd:
-			auth = (mailusername, mailpasswd)
-		secure = None
-		if mailusetls:
-			secure = ()
-		mail_handler = SMTPHandler(
-			mailhost=(mailserver, mailport),
-			fromaddr='no-reply@' + mailserver,
-			toaddrs=adminemails,
-			credentials=auth,
-			secure=secure,
-			subject='An error was logged...')
-		mail_handler.setLevel(logging.ERROR)
-		app.logger.addHandler(mail_handler)
+	from app.errors import bp as errors_bp
+	app.register_blueprint(errors_bp)
 
-	#### CONFIGURE FILE HANDLER TO LOG ERRORS ####
-	if not os.path.exists('logs'):
-		os.mkdir('logs')
-	file_handler = RotatingFileHandler(
-		'logs/microblog.log',
-		maxBytes=10240,
-		backupCount=10)
-	file_handler.setFormatter(
-		logging.Formatter(
-			'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+	from app.auth import bp as auth_bp
+	app.register_blueprint(auth_bp, url_prefix='/auth')
+
+	from app.main import bp as main_bp
+	app.register_blueprint(main_bp)
+
+	#### EMAIL SERVER PARAMS - CONFIGURE THESE AS ENVIRONMENT VARS (dotenv) ####
+	mailserver = app.config['MAIL_SERVER']
+	mailusername = app.config['MAIL_USERNAME']
+	mailpasswd = app.config['MAIL_PASSWORD']
+	mailusetls = app.config['MAIL_USE_TLS']
+	mailport = app.config['MAIL_PORT']
+	adminemails = app.config['ADMIN_EMAILS']
+
+	if not app.debug and not app.testing:
+		#### CONFIGURE SMTP EMAIL HANDLER TO REPORT ERRORS ####
+		if mailserver:
+			auth = None
+			if mailusername or mailpasswd:
+				auth = (mailusername, mailpasswd)
+			secure = None
+			if mailusetls:
+				secure = ()
+			mail_handler = SMTPHandler(
+				mailhost=(mailserver, mailport),
+				fromaddr='no-reply@' + mailserver,
+				toaddrs=adminemails,
+				credentials=auth,
+				secure=secure,
+				subject='An error was logged...')
+			mail_handler.setLevel(logging.ERROR)
+			app.logger.addHandler(mail_handler)
+
+		#### CONFIGURE FILE HANDLER TO LOG ERRORS ####
+		if not os.path.exists('logs'):
+			os.mkdir('logs')
+		file_handler = RotatingFileHandler(
+			'logs/microblog.log',
+			maxBytes=10240,
+			backupCount=10)
+		file_handler.setFormatter(
+			logging.Formatter(
+				'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+			)
 		)
-	)
-	file_handler.setLevel(logging.INFO)
-	app.logger.addHandler(file_handler)
+		file_handler.setLevel(logging.INFO)
+		app.logger.addHandler(file_handler)
 
-	app.logger.setLevel(logging.INFO)
-	app.logger.info("Microblog startup")
+		app.logger.setLevel(logging.INFO)
+		app.logger.info("Microblog startup")
+
+	return app
 
 @babel.localeselector
 def get_local():
-	return request.accept_languages.best_match(app.config['LANGUAGES'])
+	return request.accept_languages.best_match(current_app.config['LANGUAGES'])
 	# return 'es' # for testing
 
-from app import routes, models, errors
+from app import models#, routes, errors
---
+++ app/auth/forms.py
@@ -0,0 +1,40 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+
+from flask_babel import _, lazy_gettext as _l
+from app.models import User
+
+class LoginForm(FlaskForm):
+	username = StringField(_l('Username'), validators=[DataRequired()])
+	password = PasswordField(_l('Password'), validators=[DataRequired()])
+	remember_me = BooleanField(_l('Remember Me (fondly)'))
+	submit = SubmitField(_l('Sign In'))
+
+class RegistrationForm(FlaskForm):
+	username = StringField(_l('Username'), validators=[DataRequired()])
+	email = StringField(_l('Email'), validators=[DataRequired(), Email()])
+	password = StringField(_l('Password'), validators=[DataRequired()])
+	password2 = StringField(_l('Repeat Password'), validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField(_l('Register'))
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError(_l('That user name is already taken.  Please choose another username.'))
+
+	def validate_email(self,email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError(_l('That email address is already taken.  Please choose another email address'))
+
+class ResetPasswordRequestForm(FlaskForm):
+	email = StringField(_l('Email'), validators=[DataRequired(), Email()])
+	submit = SubmitField(_l('Request Password Reset'))
+
+class ResetPasswordForm(FlaskForm):
+	password = PasswordField(_l('Password'), validators=[DataRequired()])
+	password2 = PasswordField(
+		_l('Repeat Password'), validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField(_l('Request Password Reset'))
